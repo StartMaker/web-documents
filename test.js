e171cb21b38f2270cb7fa2f64a358c22fbec881b
lenovo <2019838265@qq.com>
2020-02-18 00:36:23 +0800

    'update'

--- test.js
+++
@@ -1,9 +0,0 @@
-new Promise(function (r, j) {
-  r(1)
-})
-.then(function (data) {
-  return Promise.all([Promise.resolve(1), Promise.resolve(2)])
-})
-.then(function (data) {
-  console.log(data);
-});
